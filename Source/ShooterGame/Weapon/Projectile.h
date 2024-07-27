8f3514721864f0569b1a85b703974aab4f0181d1
Marcinp98 <marcinpilipczuk98@gmail.com>
2024-07-27 21:34:57 +0200

    add projectile hit events, spawning particles and playing impact sound

--- Source/ShooterGame/Weapon/Projectile.h
+++ Source/ShooterGame/Weapon/Projectile.h
@@ -15,8 +15,13 @@ public:
 	AProjectile();
 	virtual void Tick(float DeltaTime) override;
 
+	virtual void Destroyed() override;
+
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 private:
 	UPROPERTY(EditAnywhere)
@@ -30,6 +35,11 @@ private:
 
 	class UParticleSystemComponent* TracerComponent;
 
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ImpactSound;
 public:	
 
 
