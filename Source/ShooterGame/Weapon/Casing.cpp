0651f23d4f8f5f8e5831ef4399ae9e3c2c5afb34
Marcinp98 <marcinpilipczuk98@gmail.com>
2024-07-28 10:26:04 +0200

    Add Casing class, spawning bullet shell when shoot weapon

---
+++ Source/ShooterGame/Weapon/Casing.cpp
@@ -0,0 +1,25 @@
+
+
+
+#include "Casing.h"
+
+ACasing::ACasing()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
+	SetRootComponent(CasingMesh);
+}
+
+void ACasing::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACasing::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/ShooterGame/Weapon/Casing.h
@@ -0,0 +1,31 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Casing.generated.h"
+
+UCLASS()
+class SHOOTERGAME_API ACasing : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	ACasing();
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* CasingMesh;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:	
+
+	virtual void Tick(float DeltaTime) override;
+
+};
